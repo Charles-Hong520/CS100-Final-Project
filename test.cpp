39d7902a91e34e874b0311a8fd66910cfb698168
KunalMittal322 <mittal.kunal01@gmail.com>
2020-11-24 13:32:51 -0800

    Preliminary outline of a test

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+#include "Busyness.hpp"
+#include "DayIndex.hpp"
+#include "WeekIndex.hpp"
+#include "MonthIndex.hpp"
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
