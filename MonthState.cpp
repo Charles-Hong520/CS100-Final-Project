7a21f57e5e58fd6cdfde76134dba0dc5ca329989
zhiyuanwen <zwen012@ucr.edu>
2020-12-08 13:01:36 -0800

    Month printing

--- MonthState.cpp
+++ MonthState.cpp
@@ -1,5 +1,21 @@
 #include "MonthState.hpp"
 
 void MonthState::update(int num) {
-    
+    int daysToMonth[] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int count = 1;
+    int totalDays = 0;
+    for(i = 0; i < num; ++i) {
+	totalDays += daysToMonth[i];
+    }
+
+    for(i = 0; i < daysToMonth.at(num - 1); ++i) {
+	cout << num << "/" << i + 1 << endl;   
+
+    	if(calendar.getDay(totalDays).size() != 0) {
+	    for(auto ev : calendar.at(totalDays)) {
+	    	cout << count << ". " << ev -> getName() << endl;
+	    	count++;
+	    }
+        }
+    }
 }
